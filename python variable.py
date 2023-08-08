c643b89615a3f61ef7666ff24d3cf6168cfeab51
2004manish <124455399+2004manish@users.noreply.github.com>
2023-08-09 00:12:30 +0530

    Add files via upload

---
+++ python variable.py
@@ -0,0 +1,29 @@
+# The variables are containers to the store data like numbers and strings
+# 1.create variables
+# 2.print variables
+# 3.change value assigned to variables
+x = 24 # x is variable and 25 it is storing an integer value 25
+print (x)
+x = 3 # 3 is a integer value
+x = 'mobile' # mobile is a string
+print(x)
+# TYPE()
+x =4
+y="moble"
+print(type(x))
+print(type(y))
+# Multiple variables
+fruits =["apple","banana","cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+# Create a variable outside of a function, and use it inside the function
+x = "awesome"
+
+
+
