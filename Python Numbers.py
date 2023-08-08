723aa09614d96269ee46c207c5f2f6876f67c2db
2004manish <124455399+2004manish@users.noreply.github.com>
2023-08-09 00:12:55 +0530

    Add files via upload

---
+++ Python Numbers.py
@@ -0,0 +1,57 @@
+# types
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+# Integers:
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+# float
+x= 5.2
+y= 1.0
+z= -20.11
+print(type(x))
+print(type(y))
+print(type(z))
+# complex
+x = 3+54j
+y = 65j
+z = -42j
+print(type(x))
+print(type(y))
+print(type(z))
+# Convert from one type to another:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+# convert from int to float:
+a = float(x)
+
+# convert from float to int:
+b = int(y)
+
+# convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+# Import the random module, and display a random number between 1 and 9:
+import random
+
+print(random.randrange(1, 10))
+
+
+
+
+
+
