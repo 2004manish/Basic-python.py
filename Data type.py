4cec1a664388ebc2c35cf1bfd3931c1e6e3bfc68
2004manish <124455399+2004manish@users.noreply.github.com>
2023-08-09 00:13:23 +0530

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,76 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type: Nonetype
+x = "Hello world"
+print(type(x))
+# Int
+x = 20
+
+#  display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+# Float
+x = 20.5
+print(x)
+print(type(x))
+# complex
+x = 2j
+print(x)
+print(type(x))
+# list
+x = ["apple","banana","cherry"]
+print(x)
+print(type(x))
+# tuple
+x = ("apple","banana","cherry")
+print(x)
+print(type(x))
+# range
+x = range(10)
+print(x)
+print(type(x))
+# dict
+x = {"name" : "John", "age" : 36}
+
+# display x:
+print(x)
+
+# display the data type of x:
+print(type(x))
+print(x['name'])
+print(x['age'])
+# set
+x = {"apple","banana","cherry"}
+print(x)
+print(type(x))
+# frozenset
+x = frozenset({"apple","banana","cherry"})
+print(x)
+print(type(x))
+# bool
+x = True
+print(x)
+print(type(x))
+# bytes
+x = b"Hello world"
+print(x)
+print(type(x))
+# bytearray
+x =bytearray(4)
+print(x)
+print(type(x))
+# Memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
+# none types
+x = None
+print(x)
+print(type(x))
